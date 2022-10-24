b93a47a1dc03548cc121ad52d1d3062b63d68998
guilhermeferreira7 <guilhermefribas@outlook.com>
2022-10-24 15:28:44 -0300

    added enterprise home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeStudentComponent } from './home-student/home-student.component';
+import { HomeEnterpriseComponent } from './home-enterprise/home-enterprise.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'aluno/:id/inicio', component: HomeStudentComponent },
+  { path: 'empresa/:id/inicio', component: HomeEnterpriseComponent },
 ];
 
 @NgModule({
